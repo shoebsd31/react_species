d43431f7ae94068fa1d159c14ba9ea738e3e7ebf
shoeb <shoebsd31@gmail.com>
2019-10-23 13:02:26 +0200

    removed loader from regions

--- src/components/regions/Regions.js
+++ src/components/regions/Regions.js
@@ -11,21 +11,12 @@ useEffect(() => {
     getRegions().then((_regions) => {
       setRegions(_regions.results)});
 }, []);
-if(regions.length>0)
-{
     return (
   <div className="jumbotron">
     <h1>Regions</h1>
     <RegionList regions={regions} />
   </div>
-)}
-else {
-  return(
-    <div>
-      <h1>Fetching Results...</h1>
-    </div>
-  );
-}
+)
 };
 
 export default RegionsPage;
